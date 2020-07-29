b3d523e13f060cf49e9c7af24ea9a06435cc9cf1
hall-geeks <hall-geeks@hallgeeks>
2020-07-29 17:36:05 +0530

    Spring Security

---
+++ springsecurity-jwtex/src/main/java/com/springBoot/springsecurityjwtex/SpringsecurityJwtexApplication.java
@@ -0,0 +1,13 @@
+package com.springBoot.springsecurityjwtex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringsecurityJwtexApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringsecurityJwtexApplication.class, args);
+	}
+
+}
---
+++ springsecurity-jwtex/src/test/java/com/springBoot/springsecurityjwtex/SpringsecurityJwtexApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springBoot.springsecurityjwtex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringsecurityJwtexApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
